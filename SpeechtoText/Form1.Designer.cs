de3b7e998b5cf0f7e59fed69ff05c462447ebe01
Salih Dursun <salihldursun@gmail.com>
2025-11-01 20:09:40 +0300

    Initial commit

---
+++ SpeechtoText/Form1.Designer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace SpeechtoText
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 259);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
+
---
+++ SpeechtoText/Form1.cs
@@ -0,0 +1,82 @@
+﻿
+using System;
+using System.Windows.Forms;
+
+
+using System.Speech.Recognition;
+using System.Globalization;     
+using System.Diagnostics;       
+
+
+namespace SpeechtoText 
+{
+    public partial class Form1 : Form
+    {
+       
+        private SpeechRecognitionEngine speechEngine;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            BaslatDinleme();
+        }
+        
+        public void BaslatDinleme()
+        {
+            try
+            {
+                
+                CultureInfo cultureInfo = new CultureInfo("tr-TR");
+                speechEngine = new SpeechRecognitionEngine(cultureInfo);
+
+               
+                speechEngine.SetInputToDefaultAudioDevice();
+
+               
+                speechEngine.LoadGrammar(new DictationGrammar());
+
+                speechEngine.SpeechRecognized += SpeechEngine_SpeechRecognized;
+
+                
+                speechEngine.SpeechRecognitionRejected += SpeechEngine_SpeechRecognitionRejected;
+
+                
+                speechEngine.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch (Exception ex)
+            {
+                
+                MessageBox.Show("Konuşma tanıma başlatılamadı.\nHata: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        
+        private void SpeechEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+           
+            string algilananMetin = e.Result.Text;
+
+            
+            float guvenilirlik = e.Result.Confidence;
+
+           
+            Debug.WriteLine($"Algılanan Metin: {algilananMetin} (Güvenilirlik: {guvenilirlik:P0})");
+
+            
+        }
+
+    
+        private void SpeechEngine_SpeechRecognitionRejected(object sender, SpeechRecognitionRejectedEventArgs e)
+        {
+       
+            Debug.WriteLine("Konuşma algılanamadı veya anlaşılamadı.");
+        }
+    }
+}
